79ec53cdecf6bd0da87265d58016aaab1cb42269
woochoi <woochoi@gmarket.com>
2023-02-14 21:41:33 +0900

    구현체 적용
    의존성 주입

--- src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
+++ src/main/java/com/fastcampus/javaoop/logic/JavaSort.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class JavaSort <T extends Comparable<T>> {
+public class JavaSort <T extends Comparable<T>> implements Sort<T> {
+  @Override
   public List<T> sort(List<T> list) {
-    List<T> output = new ArrayList<>(list);
+    List<T> output = new ArrayList<>(list); // 변하지 않는 값으로...
     Collections.sort(output);
 
     return output;
